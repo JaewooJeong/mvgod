bbdac20679f4f0205f4086c7d0d1f99918bae41c
Jaewoo, Jeong <mvgood21@gmail.com>
2017-01-25 14:31:15 +0900

    master 1. react node 무시

---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class App extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      name: ''
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={()=> {this.setState({name: 'Velopert'});}}>Click!</button>
+        <h1>Hello React Test Page! {this.state.name}</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
